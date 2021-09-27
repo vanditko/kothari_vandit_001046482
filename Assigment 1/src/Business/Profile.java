1b7aa3f69e1029bf2820d9f3c3d96a242a806c72
vanditko <kothari.vandit12345@gmail.com>
2021-09-27 09:16:30 +0530

    Assigment1

---
+++ Assigment 1/src/Business/Profile.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author hp
+ */
+public class Profile {
+    private String name;
+    private String Geodata;
+    private String dob ;
+    private int telenum;
+    private int faxnum;
+    private String email;
+    private int SSN;
+    private int medinum;
+    private int healthnum;
+    private String accnum;
+    private String licnum;
+    private String platenum;
+    private String serialnum;
+    private String linkedin;
+    private String ipadd;
+    private String uninum ;
+   
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeodata() {
+        return Geodata;
+    }
+
+    public void setGeodata(String Geodata) {
+        this.Geodata = Geodata;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getTelenum() {
+        return telenum;
+    }
+
+    public void setTelenum(int telenum) {
+        this.telenum = telenum;
+    }
+
+    public int getFaxnum() {
+        return faxnum;
+    }
+
+    public void setFaxnum(int faxnum) {
+        this.faxnum = faxnum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getMedinum() {
+        return medinum;
+    }
+
+    public void setMedinum(int medinum) {
+        this.medinum = medinum;
+    }
+
+    public int getHealthnum() {
+        return healthnum;
+    }
+
+    public void setHealthnum(int healthnum) {
+        this.healthnum = healthnum;
+    }
+
+    public String getAccnum() {
+        return accnum;
+    }
+
+    public void setAccnum(String accnum) {
+        this.accnum = accnum;
+    }
+
+    public String getLicnum() {
+        return licnum;
+    }
+
+    public void setLicnum(String licnum) {
+        this.licnum = licnum;
+    }
+
+    public String getPlatenum() {
+        return platenum;
+    }
+
+    public void setPlatenum(String platenum) {
+        this.platenum = platenum;
+    }
+
+    public String getSerialnum() {
+        return serialnum;
+    }
+
+    public void setSerialnum(String serialnum) {
+        this.serialnum = serialnum;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpadd() {
+        return ipadd;
+    }
+
+    public void setIpadd(String ipadd) {
+        this.ipadd = ipadd;
+    }
+
+    public String getUninum() {
+        return uninum;
+    }
+
+    public void setUninum(String uninum) {
+        this.uninum = uninum;
+    }
+
+    
+
+}
